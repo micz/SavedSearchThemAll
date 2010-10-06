4293826c4c9276b5d7d6e4a00f3251157daf260f
mic <m@micz.it>
2010-10-07 00:58:35 +0200

    other accounts' root folders are no more selected

--- chrome/content/overlay.js
+++ chrome/content/overlay.js
@@ -87,24 +87,19 @@ generateFoldersToSearchList: function(server)
 {
   var uriSearchString = "";
   
-  var accountManager = Components.classes["@mozilla.org/messenger/account-manager;1"].getService(Components.interfaces.nsIMsgAccountManager);
-  var allServers = accountManager.allServers;
-  var numServers = allServers.Count();
-  for (var index = 0; index < numServers; index++)
-  {
-    var rootFolder  = allServers.GetElementAt(index).QueryInterface(Components.interfaces.nsIMsgIncomingServer).rootFolder;
+  
+    var rootFolder  = server.QueryInterface(Components.interfaces.nsIMsgIncomingServer).rootFolder;
     if (rootFolder)
     {
       uriSearchString = this.processSearchSettingForFolder(rootFolder, uriSearchString);
       var allFolders = Components.classes["@mozilla.org/supports-array;1"].createInstance(Components.interfaces.nsISupportsArray);
       rootFolder.ListDescendents(allFolders);
       var numFolders = allFolders.Count();
-      for (var folderIndex = 0; folderIndex < numFolders; folderIndex++){
+      for (var folderIndex = 0; folderIndex < numFolders; folderIndex++){ 
           var curr_folder=allFolders.GetElementAt(folderIndex).QueryInterface(Components.interfaces.nsIMsgFolder);
-          if(!(curr_folder.flags & nsMsgFolderFlags.Virtual)&&curr_folder.server==server) uriSearchString = this.processSearchSettingForFolder(curr_folder, uriSearchString);
+          if(!(curr_folder.flags & nsMsgFolderFlags.Virtual)&&(curr_folder.server==server)) uriSearchString = this.processSearchSettingForFolder(curr_folder, uriSearchString);
         }
-    }
-  } // for each account
+}
 
   return uriSearchString;
 },
