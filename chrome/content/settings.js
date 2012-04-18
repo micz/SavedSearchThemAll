26f1a2c6599c907aa0074cd1554229532f88dcc9
mic <m@micz.it>
2012-04-18 23:43:47 +0200

    REimplemented the ConsiderOnlySubfolders exclusion for the AllFromLocalFolders option

---
+++ chrome/content/settings.js
@@ -0,0 +1,13 @@
+if(!it) var it={};
+if(!it.micz) it.micz={};
+if(!it.micz.SavedSearchThemAllPref) it.micz.SavedSearchThemAllPref={};
+ 
+it.micz.SavedSearchThemAllPref = {
+  AllFromLocalFolders_reset: function(){
+    var strbundle = document.getElementById("SavedSearchThemAll-string-bundle");
+    var prefs = Components.classes["@mozilla.org/preferences-service;1"].getService(Components.interfaces.nsIPrefService);
+    prefs = prefs.getBranch("extensions.SavedSearchThemAll.");
+    prefs.setBoolPref("AllFromLocalFolders",false);
+    document.getElementById("SavedSearchThemAll.AllFromLocalFolders_checkbox").disabled=!document.getElementById("SavedSearchThemAll.AllFromLocalFolders_checkbox").disabled;
+  },
+};
