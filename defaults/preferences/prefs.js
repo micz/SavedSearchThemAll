96fc4af1da1ed1dd0624e755252db0e1d4aa7aba
Mic <m@micz.it>
2012-04-18 16:54:57 +0200

    added ConsiderOnlySubfolders option

--- defaults/preferences/prefs.js
+++ defaults/preferences/prefs.js
@@ -1,6 +1,2 @@
-//pref("extensions.SavedSearchThemAll.boolpref", false);
-//pref("extensions.SavedSearchThemAll.intpref", 0);
-//pref("extensions.SavedSearchThemAll.stringpref", "A string");
-
-// https://developer.mozilla.org/en/Localizing_extension_descriptions
-pref("extensions.savedsearchthemall@micz.it.description", "chrome://SavedSearchThemAll/locale/overlay.properties");pref("extensions.SavedSearchThemAll.AllFromLocalFolders", false);
+pref("extensions.SavedSearchThemAll.AllFromLocalFolders", false);
+pref("extensions.SavedSearchThemAll.ConsiderOnlySubfolders", false);
