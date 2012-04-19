0a84562fe92e2b6f9c2a8ca1b67b342efbe762e4
mic <m@micz.it>
2012-04-19 20:49:31 +0200

    correct disabling options when opening settings window

--- chrome/content/settings.js
+++ chrome/content/settings.js
@@ -3,9 +3,15 @@ if(!it.micz) it.micz={};
 if(!it.micz.SavedSearchThemAllPref) it.micz.SavedSearchThemAllPref={};
  
 it.micz.SavedSearchThemAllPref = {
+
+onLoad: function() {
+    let prefs = Components.classes["@mozilla.org/preferences-service;1"].getService(Components.interfaces.nsIPrefService);
+    prefs = prefs.getBranch("extensions.SavedSearchThemAll.");
+    document.getElementById("SavedSearchThemAll.AllFromLocalFolders_checkbox").disabled=prefs.getBoolPref("ConsiderOnlySubfolders");  
+  },
+
   AllFromLocalFolders_reset: function(){
-    var strbundle = document.getElementById("SavedSearchThemAll-string-bundle");
-    var prefs = Components.classes["@mozilla.org/preferences-service;1"].getService(Components.interfaces.nsIPrefService);
+    let prefs = Components.classes["@mozilla.org/preferences-service;1"].getService(Components.interfaces.nsIPrefService);
     prefs = prefs.getBranch("extensions.SavedSearchThemAll.");
     prefs.setBoolPref("AllFromLocalFolders",false);
     document.getElementById("SavedSearchThemAll.AllFromLocalFolders_checkbox").disabled=prefs.getBoolPref("ConsiderOnlySubfolders");
